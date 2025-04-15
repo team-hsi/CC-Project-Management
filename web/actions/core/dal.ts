223f1c941b3ebce59a5fc303423cc02fddf2bf3e
Iman Mohammedsaeed <imanahmedmohs@gmail.com>
2025-04-15 00:56:00 +0000

    removed unwated functions

--- web/actions/core/dal.ts
+++ web/actions/core/dal.ts
@@ -33,18 +33,6 @@ export const getSession = cache(async () => {
   return session;
 });
 
-// export const getActiveWS = cache(async () => {
-//   const cookie = (await cookies()).get(ACTIVE_WS)?.value;
-//   if (!cookie) {
-//     redirect("/select-workspace");
-//   }
-//   return cookie;
-// });
-
-// export const deleteActiveWS = async () => {
-//   const cookieStore = await cookies();
-//   cookieStore.delete(ACTIVE_WS);
-// };
 
 export const getUser = cache(async () => {
   const session = await getSession();
